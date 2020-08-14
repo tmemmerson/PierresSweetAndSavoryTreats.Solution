77c5e4d80f0fb6d3ae458ce0398dab1fd593f4ca
Tristan Emmerson <tristan@stickerslug.com>
2020-08-14 11:49:58 -0700

    create login ViewModel for authentication

---
+++ PierresSweetAndSavoryTreats/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+namespace PierresSweetAndSavoryTreats.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
