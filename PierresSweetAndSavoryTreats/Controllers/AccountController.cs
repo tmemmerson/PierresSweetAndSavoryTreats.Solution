e5b00757dd8d28c3fcf9804ad4fe804e95e3f390
Tristan Emmerson <tristan@stickerslug.com>
2020-08-14 12:27:34 -0700

    refactor

--- PierresSweetAndSavoryTreats/Controllers/AccountController.cs
+++ PierresSweetAndSavoryTreats/Controllers/AccountController.cs
@@ -12,7 +12,7 @@ namespace PierresSweetAndSavoryTreats.Controllers
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly SignInManager<ApplicationUser> _signInManager;
 
-    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ToDoListContext db)
+    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, PierresSweetAndSavoryTreatsContext db)
     {
       _userManager = userManager;
       _signInManager = signInManager;
